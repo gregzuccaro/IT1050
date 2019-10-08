435d75f60fb62d05b9f4e59e3cef2bf276d6265e
Greg Zuccaro <gregzuccaro@gmail.com>
2019-10-07 21:42:54 -0400

    add

---
+++ Practice Exercises/Practice/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice
+{
+    class Person
+    {
+        //Member Variable Declaration
+        //Instance Variable Declaration
+
+        public string FirstName;
+        public string MiddleIntitial;
+        public string LastName;
+
+        public string GetFullName();
+        {
+            return this.FirstName + " " + this.MiddleInitial + ". " + this.LastName;
+
+
+        }
+
+    }
+}
---
+++ Practice Exercises/Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person greggie = new Person();
+            greggie.FirstName = "Gregory";
+            greggie.MiddleInitial = "J";
+            greggie.LastName = "Zuccaro";
+
+
+            Person farley = new Person();
+            farley.FirstName = "Brandon";
+            farley.MiddleInitial = "A";
+            farley.LastName = "Farley";
+
+            Console.WriteLine(greggie.GetFullName());
+
+
+        }
+    }
+}
