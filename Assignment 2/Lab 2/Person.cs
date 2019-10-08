34b82063cf35d7fc6e524270424967154ae4d2f5
Greg Zuccaro <gregzuccaro@gmail.com>
2019-10-08 14:16:13 -0400

    add

---
+++ Assignment 2/Lab 2/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab_2
+{
+    class Person
+    {
+        //Instance - Member variable declaration
+        public int age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+
+      
+        public void PrintNameAndAge()
+        {
+
+
+
+        }
+
+    }
+
+
+}
