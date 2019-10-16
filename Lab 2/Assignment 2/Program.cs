8e64659a7f29a0193df7a0cd8a131d5ff044ff8d
gregzuccaro <gregzuccaro@gmail.com>
2019-10-16 15:10:22 -0400

    add

---
+++ Lab 2/Assignment 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            p1.GatherDataFromUser();
+            p1.CreateSpouseIfMarried();
+            p1.PrintNameAndAge();
+            
+            Person p2 = new Person();
+            p2.GatherDataFromUser();
+            p2.CreateSpouseIfMarried();
+            p2.PrintNameAndAge();
+        }
+    }
+}
