ee30586e68cfb643fb7643a3f0581871852571f1
Greg Zuccaro <gregzuccaro@gmail.com>
2019-10-27 16:44:54 -0400

    add

---
+++ Practice 2/Practice 2/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public int AddTwoIntegers(int theFirstNumber, int theSecondNumber)
+        {
+            return theFirstNumber + theSecondNumber;
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Practice 2/Practice 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person griggie = new Person();
+            griggie.FirstName = "Greg";
+            griggie.LastName = "Zuccaro";
+            griggie.Age = 29;
+
+            int sum1 = griggie.AddTwoIntegers(9, 2);
+            int sum2 = griggie.AddTwoIntegers(5, 9);
+            int sum3 = griggie.AddTwoIntegers(7, 7);
+
+
+
+           System.Console.WriteLine("The Name of griggie is: " + griggie.GetFullName());
+            System.Console.WriteLine("The result of the integers that griggie added together is: " + sum1 + " " + sum2 + " " +  sum3);
+            System.Console.ReadKey();
+
+
+        }
+    }
+}
