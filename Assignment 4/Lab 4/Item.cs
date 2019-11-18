693972ed4506f9097aa1a8a57c88697a7ff4cd33
gregzuccaro <gregzuccaro@gmail.com>
2019-11-18 16:10:57 -0500

    Add files via upload

---
+++ Assignment 4/Lab 4/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab_4
+{
+    class Item
+    {
+       //declare member variables
+
+       private string name = "";
+       private double price = 0;
+       private int quantity = 0;
+       
+        //initialize constructor
+        public Item(string itemName, double itemPrice, int itemQuantity)
+        {
+            itemName = this.name;
+            itemPrice = this.price;
+            itemQuantity = this.quantity;
+        }
+
+
+    
+    }
+}
