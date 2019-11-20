b8feb10cebf11766b0849b0d63d58f486a0be986
Greg Zuccaro <gregzuccaro@gmail.com>
2019-11-19 22:22:06 -0500

    add

---
+++ Counts and Headways/Counts and Headways/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Counts_and_Headways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello and Welcome!");
+            System.Console.Write("How many passengers were on the bus?       ");
+            int numberOfPassengers = int.Parse(Console.ReadLine());
+
+                   if (numberOfPassengers == 0)
+                    {
+                System.Console.WriteLine("The Bus is empty today!  Let's designate this as Category A which means that there were zero passengers on the bus. ");
+                    }
+                    else if (numberOfPassengers > 1 && numberOfPassengers < 5)
+                    {
+                System.Console.WriteLine("Wow, small turn-out today.  Let's designate this as Category B which means that there were a small number of passengers on the bus.");
+                   else if (numberOfPassengers > 5 && numberOfPassengers < 10)
+                      {
+                    System.Console.WriteLine("OK, Medium turn-out today.  Let's designate this as Category C which means that there were a medium number of passengers on the bus.")
+                        
+
+                   else if (numberOfPassengers > 10)
+                {
+                        System.Console.WriteLine("All Aboard! A large turn-out today.  Let's designate this as Category D which means that there were a large number of passengers on the bus.");
+
+                   
+
+
+                }
+
+
+            }
+
+
+
+
+
+    }
+}
